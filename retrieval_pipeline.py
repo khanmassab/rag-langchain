9f8d20e49f0abf53be61dc11f32c759d4ad4b317
Massab Khan <massab725@gmail.com>
2026-01-31 00:09:01 +0500

    chat with history:

--- retrieval_pipeline.py
+++ retrieval_pipeline.py
@@ -4,9 +4,7 @@ from langchain_ollama import OllamaEmbeddings, ChatOllama
 EMBEDDING_MODEL="mxbai-embed-large"
 PERSIST_DIRECTORY="db/chroma"
 
-def get_relevant_docs():
-    query=input("What do you want to known: ") 
-
+def get_relevant_docs(query):
     embeddings=OllamaEmbeddings(model=EMBEDDING_MODEL) 
 
     chroma = Chroma(
@@ -27,10 +25,10 @@ def get_relevant_docs():
 
     relevant_docs = retriever.invoke(query)
 
-    return relevant_docs, query
+    return relevant_docs 
 
 def refine_output_with_llm(relevant_docs, query):
-
+    
     llm = ChatOllama(
         model="qwen:7b",
         temperature=0
@@ -38,21 +36,36 @@ def refine_output_with_llm(relevant_docs, query):
 
     messages = [
         ("system", "You're a assistant and you'll help me get the best answer out of five or less docs with refined output to user only if the answer is accurate and available in the docs with respect to the query that you'll be provided. If the relevant docs don't have matching answer to the query just say that you cannot assist with queries out of the scope of the system"),
-        ("human", f"Documents: {relevant_docs} \n Query: {query}")
+        ("human", f"Documents: {relevant_docs} \n Query with history (if applicable): {query}")
     ]
-
-    return llm.invoke(messages)
     
+    return llm.invoke(messages) 
 
 def main():
-    relevant_docs, query =get_relevant_docs()
+    history = []
+    
+    while True:
+        query=input("What do you want to known: ") 
+    
+        #check if there's a command
+        if query == "/quit":
+            return 1
+        elif query[:1] == "/":
+            return "Invalid command provided."
+    
+
+        if history and len(history) > 0:
+            query.append = ("prompt_history", f"You're to remember the prompt history in this chat and answer based on the following context: {history}")
+    
+        relevant_docs = get_relevant_docs(query=query)
    
-    response = refine_output_with_llm(
+        response = refine_output_with_llm(
             relevant_docs=relevant_docs,                      
             query=query
-    )
+        ) 
 
-    print(response.content)
-
+        print(response.content)
+    
+    print("Thank you for using this chatbot")
 if __name__=="__main__":
     main()
